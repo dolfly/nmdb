54d9e83e20b123811b1abef1f8cea9fd4a419def
Alberto Bertogli <albertito@gmail.com>
2007-06-01 15:20:16 -0300

    Remove unused constant in tcp.h.
    Luckily we don't have any fixed limit on the amount of TCP connections
    we can handle.
    
    Signed-off-by: Alberto Bertogli <albertito@gmail.com>

--- nmdb/tcp.h
+++ nmdb/tcp.h
@@ -2,9 +2,6 @@
 #ifndef _TCP_H
 #define _TCP_H
 
-/* Maximum number of TCP connections */
-#define MAX_TCPFD 2048
-
 int tcp_init(void);
 void tcp_close(int fd);
 void tcp_newconnection(int fd, short event, void *arg);
