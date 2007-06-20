8b7924ec486bc96a2a03cc34a44b786a4d182059
Alberto Bertogli <albertito@gmail.com>
2007-06-20 18:58:48 -0300

    Fix argument number in tests/c/get.c.
    Signed-off-by: Alberto Bertogli <albertito@gmail.com>

--- tests/c/get.c
+++ tests/c/get.c
@@ -18,7 +18,7 @@ int main(int argc, char **argv)
 	unsigned long g_elapsed, misses = 0;
 	nmdb_t *db;
 
-	if (argc != 4) {
+	if (argc != 3) {
 		printf("Usage: get-* TIMES KSIZE\n");
 		return 1;
 	}
